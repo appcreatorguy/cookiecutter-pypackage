d463c7d6e9312dccf42090600eeda1d494a00df8
appcreatorguy <manmenonsense@gmail.com>
2021-08-07 17:22:06 +0100

    add dependabot auto-merge

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -21,8 +21,12 @@ if __name__ == "__main__":
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
 
+    if "{{ cookiecutter.automerge_dependabot_pull_requests }}" != "y":
+        remove_file(".github/auto-merge.yml")
+        remove_file(".github/workflows/auto-merge.yml")
+
     print(
-        "Project created! Remember to add a PYPI_TOKEN or TESTPYPI_TOKEN secret to your github repository."
+        "Project created! Remember to add a PYPI_TOKEN or TESTPYPI_TOKEN, and a AUTO_MERGE secret to your github repository if you are using them."
     )
     print(
         "Also remember to switch to using the real PyPi index in .github/workflows/deploy.yml by using PYPI_TOKEN, as the TestPyPi has been used for now."
